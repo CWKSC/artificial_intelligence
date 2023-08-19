0c6ca1d154b62b9e58267ab7a4b4ab7f55bbd788
CWKSC <53114952+CWKSC@users.noreply.github.com>
2023-08-19 23:46:47 +0800

    Update

--- build_venv.ps1
+++ build_venv.ps1
@@ -1,4 +1,4 @@
-python -m venv venv
+py -3.9 -m venv venv
 .\venv\Scripts\activate
 
 python -m pip install --upgrade pip
@@ -6,13 +6,17 @@ pip install numpy
 pip install matplotlib
 pip install pandas
 pip install tqdm
-
 pip install seaborn
+
 pip install scikit-learn
 pip install scikit-optimize
+
+pip install tensorflow
+
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install torchdata
 pip install torchtext
+
 pip install spacy
 pip install portalocker
 pip install ray[tune]
--- install.ps1
+++ install.ps1
@@ -1,3 +1,4 @@
 .\venv\Scripts\activate
 
 pip install -e .\data_processing
+pip install -e .\artificial_neural_network
